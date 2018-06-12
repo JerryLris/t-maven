a13da12bef29bc3434759ba9d514fd6d25d3dbfd
JerryLris <1471142959@qq.com>
2018-06-12 09:33:50 +0800

    6-10

--- src/test/java/com/lris/test/PearlJdbcTest.java
+++ src/test/java/com/lris/test/PearlJdbcTest.java
@@ -9,4 +9,29 @@ import java.sql.SQLException;
 public class PearlJdbcTest {
 
 	
+	public static void main(String[] args) {
+		Connection conn = null;
+		PreparedStatement statement = null;
+		ResultSet resultSet = null;
+
+		try {
+			 conn = Db.getConnection();
+			String sql = "select count(1) as num from pearl.pearl where id >?";
+			statement = conn.prepareStatement(sql);
+			statement.setInt(1, 1);
+			resultSet = statement.executeQuery();
+			while (resultSet.next()) {
+				System.out.println(resultSet.getInt("num"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			Db.close(conn, statement, resultSet);
+		}
+	}
+	
+	public void insert() {
+		
+	}
+	
 }
